bd452e31a4508ee59bb15e8fb4681c5cb08631c8
Michael Amrhein <michael@adrhinum.de>
2022-01-03 17:03:04 +0100

    Renamed variables and simplified expressions.

--- qty-macros/src/lib.rs
+++ qty-macros/src/lib.rs
@@ -21,20 +21,20 @@ use ::syn::{parse_macro_input, Ident, ItemEnum};
 pub fn derive_variants_as_constants(input: TokenStream) -> TokenStream {
     let enum_def = parse_macro_input!(input as ItemEnum);
     let mut output = TokenStream2::new();
-    let enum_id = enum_def.ident;
+    let enum_ident = enum_def.ident;
     // check and gather const declarations for variants
     for variant in enum_def.variants {
-        if variant.fields.len() != 0 {
+        if !variant.fields.is_empty() {
             panic!("The given enum must be a fieldless enum.");
         }
-        let variant_id = variant.ident;
-        let const_id = Ident::new(
-            variant_id.to_string().to_case(Case::UpperSnake).as_str(),
+        let variant_ident = variant.ident;
+        let const_ident = Ident::new(
+            variant_ident.to_string().to_case(Case::UpperSnake).as_str(),
             Span::call_site(),
         );
         output = quote!(
             #output
-            pub const #const_id: #enum_id = #enum_id::#variant_id;
+            pub const #const_ident: #enum_ident = #enum_ident::#variant_ident;
         );
     }
     output.into()
@@ -43,24 +43,24 @@ pub fn derive_variants_as_constants(input: TokenStream) -> TokenStream {
 #[proc_macro_derive(EnumIter)]
 pub fn derive_enum_iter(input: TokenStream) -> TokenStream {
     let enum_def = parse_macro_input!(input as ItemEnum);
-    let enum_id = enum_def.ident;
+    let enum_ident = enum_def.ident;
     let mut output = TokenStream2::new();
     // check and gather variants
     for variant in &enum_def.variants {
-        if variant.fields.len() != 0 {
+        if !variant.fields.is_empty() {
             panic!("The given enum must be a fieldless enum.");
         }
-        let variant_id = &variant.ident;
+        let variant_ident = &variant.ident;
         output = quote!(
             #output
-            #enum_id::#variant_id,
+            #enum_ident::#variant_ident,
         );
     }
     let n_variants = &enum_def.variants.len();
     // create impl for fn iter
     output = quote!(
-        impl #enum_id {
-            const VARIANTS: [#enum_id; #n_variants] = [#output];
+        impl #enum_ident {
+            const VARIANTS: [#enum_ident; #n_variants] = [#output];
             #[doc = "Returns an iterator over the variants of `Self`."]
             #[inline(always)]
             pub fn iter() -> core::slice::Iter<'static, Self> {
