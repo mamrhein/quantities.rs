7ea45c60f3d02e0ee9c6527b16b3cb751e3f3175
Michael Amrhein <michael@adrhinum.de>
2025-04-21 08:30:43 +0200

    Added Speed unit Knot.

--- src/speed.rs
+++ src/speed.rs
@@ -12,9 +12,15 @@
 use crate::{duration::Duration, length::Length, prelude::*};
 
 #[quantity(Length / Duration)]
-#[ref_unit(Meter_per_Second, "m/s", NONE, "Reference unit of quantity `Speed`")]
+#[ref_unit(
+    Meter_per_Second,
+    "m/s",
+    NONE,
+    "Reference unit of quantity `Speed`"
+)]
 #[unit(Kilometer_per_Hour, "km/h", 0.2777777777777778, "km/h")]
 #[unit(Miles_per_Hour, "mph", 0.44704, "mi/h")]
+#[unit(Knot, "kn", 0.5144444444444445, "nmi/h")]
 /// Magnitude of the change of an objects position per unit of time
 ///
 /// Definition: Length/Duration
@@ -23,10 +29,11 @@ use crate::{duration::Duration, length::Length, prelude::*};
 ///
 /// Predefined units:
 ///
-/// | Symbol | Name                  | Definition        | Equivalent in 'm/s' |
-/// |--------|-----------------------|-------------------|---------------------|
-/// | km/h   | Kilometer per Hour    | km/h              | 5/18                |
-/// | mph    | Miles per Hour        | mi/h              | 0.44704             |
+/// | Symbol | Name               | Definition        | Equivalent in 'm/s' |
+/// |--------|--------------------|-------------------|---------------------|
+/// | km/h   | Kilometer per Hour | km/h              | 5/18                |
+/// | mph    | Miles per Hour     | mi/h              | 0.44704             |
+/// | kn     | Knot               | nmi/h             | 0.5144444444444445  |
 pub struct Speed {}
 
 #[cfg(test)]
