4547795d15b995d896d57371aa5effa38dc2f1d6
Phena Ildanach <phena.ildanach@gmail.com>
2026-01-30 14:30:18 -0600

    Exclude f32 and f64 features from the test script

--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TRYBUILD=overwrite cargo hack test --feature-powerset --optional-deps \
   --group-features mass,length,duration,area,volume,speed,acceleration,force,energy,power,frequency,datavolume,datathroughput,temperature \
-  --exclude-features doc
+  --exclude-features doc,f32,f64
